f7622165c67e0556a7a118d56725d96dcae8422e
Mel <119821610+luissanzaguilar@users.noreply.github.com>
2025-08-05 11:28:09 +0200

    Create test_main.py

---
+++ tests/test_main.py
@@ -0,0 +1,6 @@
+import pandas as pd
+from src.main import my_function  # Importa tu función
+
+def test_dataframe_creation():
+    df = pd.DataFrame({"A": [1, 2], "B": [3, 4]})
+    assert df.shape == (2, 2)
